3721f8441e129c9bbf41fbfb4df48e3abba9759b
BarbareBzhdev <michael.barbarin@gmail.com>
2018-04-16 22:46:13 +0200

    add test

--- srcs/tests/main.cpp
+++ srcs/tests/main.cpp
@@ -2,9 +2,12 @@
 
 #include <boost/program_options.hpp>
 #include <nlohmann/json.hpp>
+#include <Platform_bittrex.hpp>
+#include <Database.hpp>
 
 #include <fstream>
 #include <iostream>
+#include <memory>
 
 namespace po = boost::program_options;
 
@@ -31,8 +34,21 @@ int main(int argc, char **argv) {
 
         nlohmann::json arch = nlohmann::json::parse(out);
 
-        std::cout << "Read json file :" << std::endl << arch << std::endl;
-        //Platform_bittrex plate("", "");
+        //std::cout << "Read json file :" << std::endl << arch << std::endl;
+        std::cout << "Platform : " << arch["platform"] << std::endl;
+
+
+        nlohmann::json info = arch["database"];
+        nlohmann::json platform = arch["platform"][0];
+
+        Bee::Database db(info);
+
+        std::string publicKey = platform["publickey"];
+        std::string privateKey = platform["privatekey"];
+        //std::unique_ptr<Platform_bittrex> plate(new Platform_bittrex(publicKey, privateKey));
+
+
+
 
     return 0;
 }
